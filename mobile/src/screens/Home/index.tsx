677ec5f3239fe3cc98aa68988e8e3358aa51f5c5
Gilmar Soares <gilmar.santos@gmmti.com.br>
2021-12-24 10:22:50 -0300

    Corrige bug de exibição do telado no IOS

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { View, Text, LogBox } from 'react-native'
+import { View, Text, LogBox, KeyboardAvoidingView, Platform } from 'react-native'
 import { Header } from "../../componets/Header";
 import { styles } from "./style";
 import { MessageList } from "../../componets/MessageList";
@@ -11,10 +11,15 @@ import { userAuth } from "../../hooks/auth";
 export function Home() {
     const { user } = userAuth();
     return (
-        <View style={styles.container}>
-            <Header />
-            <MessageList />
-            {user ? <SendMessageForm /> : <SigninBox />}
-        </View>
+        <KeyboardAvoidingView
+            style={{ flex: 1 }}
+            behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        >
+            <View style={styles.container}>
+                <Header />
+                <MessageList />
+                {user ? <SendMessageForm /> : <SigninBox />}
+            </View>
+        </KeyboardAvoidingView>
     )
 }
